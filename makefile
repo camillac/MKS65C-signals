fe2c76f71b0d856530d26cd0d167a99684f63594
Camilla <ccheng6@stuy.edu>
2018-11-09 15:36:37 -0500

    not working yet :(

---
+++ makefile
@@ -0,0 +1,8 @@
+compile:
+	gcc sig.c
+
+run:
+	./a.out
+
+clean:
+	-rm -rf *.o a.out
---
+++ sig.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+
+static void sighandler(int signo){
+  if (signo == SIGINT){
+    printf("KILL ME\n");
+    char * errmsg = "EXITED B/C SIGINT\n";
+    int file = open("sigint.txt", O_CREAT | O_APPEND);
+    write(file, errmsg, sizeof(errmsg));
+    close(file);
+    exit(0);
+  }
+  if (signo == SIGUSR1){
+    printf("parent pid: %d\n", getppid());
+  }
+}
+
+int main(){
+  signal(SIGUSR1, sighandler);
+  signal(SIGINT, sighandler);
+
+  while (1){
+    printf("pid: %d\n", getpid());
+    sleep(1);
+  }
+}
