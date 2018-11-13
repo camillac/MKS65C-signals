9322982494ebc1577e8d442c4a5b5e63f2480a96
camillac <ccheng6@stuy.edu>
2018-11-12 20:42:20 -0500

    finished

--- sig.c
+++ sig.c
@@ -9,8 +9,8 @@ static void sighandler(int signo){
   if (signo == SIGINT){
     printf("KILL ME\n");
     char * errmsg = "EXITED B/C SIGINT\n";
-    int file = open("sigint.txt", O_CREAT | O_APPEND);
-    write(file, errmsg, sizeof(errmsg));
+    int file = open("sigint.txt", O_CREAT | O_WRONLY | O_APPEND);
+    write(file, errmsg, sizeof(char) * 19);
     close(file);
     exit(0);
   }
